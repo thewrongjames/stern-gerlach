ee569dbb100495f4dcc460614535f7377e23d990
James Wright <hello@jameswright.me>
2023-10-08 10:30:28 +1100

    Just get the canvas element once
    And then always resize that same element.

--- public/static/js/setup-window-resize-listener.js
+++ public/static/js/setup-window-resize-listener.js
@@ -1,20 +1,28 @@
 const CANVAS_ELEMENT_ID = 'canvas'
 
-function handleWindowResize() {
-  const canvasElement = document.getElementById(CANVAS_ELEMENT_ID)
-  if (canvasElement === null) {
-    console.error('Failed to get canvas element.')
-    return
+/**
+ * Make a function designed to be attached to the window as a resize event
+ * listener that syncs the size of the canvas image with the size of the canvas
+ * element.
+ * @param {HTMLCanvasElement} canvas The canvas to resize.
+ * @returns {() => void} The resize listener.
+ */
+function makeWindowResizeListener(canvas) {
+  return () => {
+    canvas.width = canvas.clientWidth
+    canvas.height = canvas.clientHeight
   }
-  if (!(canvasElement instanceof HTMLCanvasElement)) {
-    console.error('Canvas element was not a canvas element.')
-    return
-  }
-
-  canvasElement.width = canvasElement.clientWidth
-  canvasElement.height = canvasElement.clientHeight
 }
 
 export default function setupWindowResizeListener() {
-  window.addEventListener('resize', handleWindowResize)
+  const canvas = document.getElementById(CANVAS_ELEMENT_ID)
+  if (!(canvas instanceof HTMLCanvasElement)) {
+    console.error(canvas)
+    throw new Error(
+      `Expected the element with ID ${CANVAS_ELEMENT_ID} to be an ` +
+      `HTMLCanvasElement but got ${canvas}`
+    )
+  }
+
+  window.addEventListener('resize', makeWindowResizeListener(canvas))
 }
--- to-do.md
+++ to-do.md
@@ -1,3 +1,7 @@
 # To Do
 
-- Only get the canvas element once.
+- Just draw some shape to the canvas to make sure that the resizing is working.
+- Setup the draw loop.
+- Have some ability to add elements to the list of things to render from the UI
+  to make sure that works.
+- Have the ability to draw boxes and connect them with lines.
